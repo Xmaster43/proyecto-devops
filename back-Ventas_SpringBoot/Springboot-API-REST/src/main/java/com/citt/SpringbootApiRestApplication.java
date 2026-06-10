89b612a35a5c6470235bf91725cf2d86b3bd877c
Bastian Gomez <bastiancito.26@gmail.com>
2026-06-09 20:18:05 -0400

    Repitiendo trabajo git Actions

--- back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApiRestApplication {
- 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApiRestApplication.class, args);
 	}
